f40a5bb93a37910cdd6a6bf55fa68e006348a855
sookiemonster <dsooknanan20@stuy.edu>
2020-12-10 16:00:29 -0500

    Added add() method, and checkVoid() method, which checks if the argument received is a null and throws an IllegalArgumentException if it is null. Added tests for add().

--- Driver.java
+++ Driver.java
@@ -5,5 +5,10 @@ public class Driver {
     NoNullArrayList<String> a = new NoNullArrayList<String>();
     a.add("Bruh");
     System.out.println(a);
+    try {
+      a.add(null);
+    } catch (IllegalArgumentException e) {
+      System.out.println("Correct error.");
+    }
   }
 }
--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -4,4 +4,15 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList() {
     super();
   }
+
+  public boolean add(T element) {
+    this.checkVoid(element);
+    return super.add(element);
+  }
+
+  private void checkVoid(T element) throws IllegalArgumentException {
+    if (element == null) {
+      throw new IllegalArgumentException ("\nReceived: " + element + " || Cannot add null to NoNullArrayList.");
+    }
+  }
 }
