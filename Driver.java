8f2f8b064069e7bd206a7ba5921ac312f6242192
sookiemonster <dsooknanan20@stuy.edu>
2020-12-10 15:38:14 -0500

    Created Driver.java and added NoNullArrayList constructor tests.

---
+++ Driver.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Driver {
+  public static void main(String[] args) {
+    NoNullArrayList<String> a = new NoNullArrayList<String>();
+    a.add("Bruh");
+    System.out.println(a);
+  }
+}
